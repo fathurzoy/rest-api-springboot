537361a9f1730ea46e3ef296aa0fece27c863c23
Fathur Rahman <fathur.rahman@udana.id>
2022-05-22 00:30:50 +0700

    Menyederhanakan Definisi Class dengan Lombok

--- src/main/java/com/domain/models/entities/Category.java
+++ src/main/java/com/domain/models/entities/Category.java
@@ -9,9 +9,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter; 
+
+
 @Entity
 @Table(name = "tbl_categories")
-public class Category extends BaseEntity<String> implements Serializable{
+// lombok
+// @Setter
+// @Getter
+// @NoArgsConstructor
+public @Data class Category extends BaseEntity<String> implements Serializable{
   
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,21 +31,38 @@ public class Category extends BaseEntity<String> implements Serializable{
   @Column(length = 100,nullable = false, unique = true)
   private String name;
 
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  
+  // @Builder
+  // public Category(Long id, String name){
+  //   this.id = id;
+  //   this.name = name;
+  // }
 }
+
+
+// @Entity
+// @Table(name = "tbl_categories")
+// public class Category extends BaseEntity<String> implements Serializable{
+  
+//   @Id
+//   @GeneratedValue(strategy = GenerationType.IDENTITY)
+//   public Long id;
+
+//   @Column(length = 100,nullable = false, unique = true)
+//   private String name;
+
+//   public Long getId() {
+//     return id;
+//   }
+
+//   public void setId(Long id) {
+//     this.id = id;
+//   }
+
+//   public String getName() {
+//     return name;
+//   }
+
+//   public void setName(String name) {
+//     this.name = name;
+//   }
+// }
